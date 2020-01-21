7d2086c317f5da4c22939d613262b4f50e08e4bf
kannan-sl <kchinnasamy@snaplogic.com>
2020-01-21 11:35:36 -0800

    Added basic code to support Outh2 Authorization code grandtype

---
+++ src/main/java/com/snaplogic/oauth2/authcodeserver/config/EmployeeSecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.snaplogic.oauth2.authcodeserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class EmployeeSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/user/getEmployeesList")
+            .hasAnyRole("ADMIN").anyRequest().authenticated().and().formLogin()
+            .permitAll().and().logout().permitAll();
+
+        http.csrf().disable();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationMgr) throws Exception {
+        authenticationMgr.inMemoryAuthentication().withUser("snaplogic").password("snaplogic")
+            .authorities("ROLE_ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ src/test/java/com/snaplogic/oauth2/authcodeserver/AuthcodeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snaplogic.oauth2.authcodeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthcodeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
